fe98596dc1f125faa20dd3a57727f7023eee439b
Stig Sandbeck Mathisen <ssm@fnord.no>
2014-03-25 14:20:36 +0100

    typo

--- spec/classes/profile__webserver_spec.rb
+++ spec/classes/profile__webserver_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'profile::deployment' do
+describe 'profile::webserver' do
   include_context :test_environment
 
   it { should contain_class('profile::webserver') }
